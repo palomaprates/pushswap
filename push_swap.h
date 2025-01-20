cc6d4ed26436060ee620a25c70ff28bed5f6a492
palomaprates <paloma.prates108@gmail.com>
2025-01-20 10:55:27 -0300

    modified header

--- push_swap.h
+++ push_swap.h
@@ -10,4 +10,22 @@ typedef struct node
 	struct node *next;
 }Node;
 
+void add_back(Node **list, int nb);
+
+void add_front(Node **listInit, int nb);
+
+int get_list_size(Node **listInit);
+
+void print_list(Node **listInit);
+
+void remove_back(Node **listInit);
+
+void remove_front(Node **listInit);
+
+Node *get_last_node(Node **listInit);
+
+Node *get_first_node(Node **listInit);
+
+void rotate(Node **list);
+ 
 #endif
