c80edb6aa2d23f14d6cf058c152954c15fed69d0
palomaprates <paloma.prates108@gmail.com>
2025-01-22 12:42:45 -0300

    fix add_back

--- list_utils/add_back.c
+++ list_utils/add_back.c
@@ -1,28 +1,43 @@
-#include "push_swap.h"
-
+#include "../push_swap.h"
+#include "get_list_size.c"
+// #include "print_list.c"
 void add_back(Node **list, int nb)
 {
 	Node *new;
-	new = malloc(sizeof(Node));
 	Node *aux;
-	if (new)
-	{
-		new->value = nb;
-		new->next = NULL;
-		if (!*list)
-		{
-			*list = new; 
-			aux = *list;
-			new->next = aux->next;
-		}
-		else 
-		{
-			aux->next = new;
-			aux = aux->next;
-		}
-	}
+	int i;
+	int list_size;
+
+	i = -1;
+	list_size = get_list_size(list);
+	new = malloc(sizeof(Node));
+	if (!new)
+		return;
+	new->value = nb;
+	new->next = NULL;
+	aux = *list;
+	if (list_size == 0)
+		*list = new;
 	else
 	{
-		printf("erro ao alocar memória");
+		while (++i < list_size)
+		{
+			if (i == list_size - 1)
+				aux->next = new;
+			else
+				aux = aux->next;
+		}
 	}
 }
+
+// int main()
+// {
+// 	Node *list = NULL;
+// 	add_back(&list, 0);
+// 	add_back(&list, 1);
+// 	add_back(&list, 2);
+// 	add_back(&list, 3);
+
+// 	printf("\nLISTA:\n");
+// 	print_list(&list);
+// }
