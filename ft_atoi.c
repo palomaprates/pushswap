6c936f1fa2fa4bd292fc90c682969419f82725fc
palomaprates <paloma.prates108@gmail.com>
2025-01-25 13:44:31 -0300

    modified atoi

---
+++ ft_atoi.c
@@ -0,0 +1,25 @@
+
+long long	ft_atoi(const char *str)
+{
+	int	i;
+	long long	nbr;
+	int	negative_sign;
+
+	i = 0;
+	nbr = 0;
+	negative_sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			negative_sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + str[i] - 48;
+		i++;
+	}
+	return (nbr * negative_sign);
+}
