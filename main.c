b2ab89ecbbcc8bd4096baf419c46a434fd8d9b4e
palomaprates <paloma.prates108@gmail.com>
2025-01-27 16:08:17 -0300

    create indexArray function

--- main.c
+++ main.c
@@ -1,18 +1,50 @@
 #include "push_swap.h"
 
+void indexArray(int *arr, int size, int *result) {
+    for (int i = 0; i < size; i++) {
+        int rank = 0;
+        for (int j = 0; j < size; j++) {
+            if (arr[j] < arr[i]) {
+                rank++;
+            }
+        }
+        result[i] = rank;
+    }
+}
 int main(int argc, char *argv[])
 {
 	int i;
-	i = 1;
+	int j;
 	Node *list;
 	list = NULL;
+	Node *aux = list;
+	int size = argc - 1;
+	int arr[size];
+	int result[size];
+	i = 1;
+	j = 0;
 	if (argc <= 1)
 		return (0);
-
 	if (!create_stack(argc, argv, &list))
 	{
 		printf("Error");
 		return (0);
 	}
+	while (aux)
+	{
+		arr[i] = aux->value;
+		aux = aux->next;
+		i++;
+	}
+	indexArray(arr, size, result);
+
+	printf("Old\n");
 	print_list(&list);
+	printf("New\n");
+	while (j < size)
+	{
+		printf("%d ", result[j]);
+		j++;
+	}
+	printf("\n");
 }
