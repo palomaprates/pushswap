355e37bfae0a761ef7853f646425803d80e4e7b9
palomaprates <paloma.prates108@gmail.com>
2025-01-28 18:30:53 -0300

    update functions

---
+++ list_utils/index_stack.c
@@ -0,0 +1,25 @@
+#include "../push_swap.h"
+
+void	indexStack(Node **list)
+{
+
+	Node *aux1;
+	Node *aux2;
+	int	rank;
+
+	aux1 = *list;
+	 while (aux1)
+	 {
+		int rank = 0;
+		aux2 = *list;
+
+		while (aux2)
+		{
+			if (aux2->value < aux1->value)
+					rank++;
+			aux2 = aux2->next;
+		}
+		aux1->index = rank;
+		aux1 = aux1->next;
+    }
+}
