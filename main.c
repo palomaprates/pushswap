592e80bec1ff5de504fdbbf62ad273b0357d18af
palomaprates <paloma.prates108@gmail.com>
2025-01-30 20:22:07 -0300

    main

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 int main(int argc, char *argv[])
 {
 	int j;
-	int size;
-	int *result;
+	int test;
 	Node *list;
 	j = 0;
 	list = NULL;
@@ -15,13 +14,7 @@ int main(int argc, char *argv[])
 		printf("Error");
 		return (0);
 	}
-	size = get_list_size(&list);
-	printf("size %d\n", size);
-	result = malloc(sizeof(int) * size);
-	printf("Old\n");
-	print_list(&list);
 	indexStack(&list);
-	printf("New\n");
-	print_index(&list);
-	printf("\n");
+	radix_sort(&list);
+	print_list(&list);
 }
