1de303f54b0e4b4c7f19bf46d59dbd0c74915b98
palomaprates <paloma.prates108@gmail.com>
2025-01-30 19:37:19 -0300

    update rotate function

--- movements/rotate.c
+++ movements/rotate.c
@@ -1,11 +1,14 @@
 #include "../push_swap.h"
 
-void rotate(Node **listInit)
+void rotate(Node **listInit, char c)
 {
 	Node *aux;
 	int first;
 	int last;
 	aux = *listInit;
 	remove_front(listInit);
-	add_back(listInit, aux->value, 0);
+	add_back(listInit, aux->value, aux->index);
+	// write(1, "R", 1);
+	// write(1, &c, 1);
+	// write(1, "\n", 1);
 }
