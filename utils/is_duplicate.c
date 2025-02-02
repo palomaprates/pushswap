98aac44c6eaca15fd2f1ced91c8d6170249451a7
palomaprates <paloma.prates108@gmail.com>
2025-02-02 10:24:31 -0300

    create is_duplicate function

---
+++ utils/is_duplicate.c
@@ -0,0 +1,15 @@
+#include "../push_swap.h"
+
+int	is_duplicate(char **argv, int index)
+{
+	int current;
+
+	current = index;
+	while (argv[index + 1])
+	{
+		if (ft_atoi(argv[current]) == ft_atoi(argv[index + 1]))
+			return (1);
+		index++;
+	}
+	return (0);
+}
