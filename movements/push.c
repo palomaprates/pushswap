6da08fedf57f0e63b044bf2bfb7697e662e19c96
palomaprates <paloma.prates108@gmail.com>
2025-02-04 12:37:58 -0300

    update push function

--- movements/push.c
+++ movements/push.c
@@ -8,7 +8,7 @@ void push(Node **list, Node **listB, char c)
 		return;
 	remove_front(list);
 	add_front(listB, aux->value, aux->index);
-	// write(1, "P", 1);
-	// write(1, &c, 1);
-	// write(1, "\n", 1);
+	write(1, "p", 1);
+	write(1, &c, 1);
+	write(1, "\n", 1);
 }
