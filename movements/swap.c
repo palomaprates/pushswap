3a1897ee787895072b208fde0e484856d9559efa
palomaprates <paloma.prates108@gmail.com>
2025-02-04 12:35:41 -0300

    fix swap function

--- movements/swap.c
+++ movements/swap.c
@@ -1,22 +1,24 @@
 #include "../push_swap.h"
 
-void swap(Node **listInit)
+void swap(Node **listInit, char c)
 {
-	int temp;
-	Node *aux;
-	if (get_list_size(listInit) < 2)
+	int	temp;
+	int	temp_index;
+	Node	*aux;
+	if (get_list_size(*listInit) < 2)
 		return;
 	aux = *listInit;
 	
 	temp = aux->value;
 	aux->value = aux->next->value; 
 	aux->next->value = temp;
-}
 
-void ss(Node **listA, Node **listB)
-{
-	swap(listA);
-	swap(listB);
+	temp_index = aux->index;
+	aux->index = aux->next->index;
+	aux->next->index = temp_index;
+	write(1, "s", 1);
+	write(1, &c, 1);
+	write(1, "\n", 1);
 }
 
 
