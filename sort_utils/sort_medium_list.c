d56bd6b4a9f1317078ddc94e57382d60bdf5f7b3
palomaprates <paloma.prates108@gmail.com>
2025-02-08 10:28:34 -0300

    create sort medium list function

---
+++ sort_utils/sort_medium_list.c
@@ -0,0 +1,29 @@
+#include "../push_swap.h"
+
+int	sort_medium_list(Node **list, Node **list_b, int size)
+{
+	Node *aux;
+	int less;
+	
+	aux = *list;
+	less = 0;
+	while (size > 2)
+	{
+		if (aux->index == less)
+		{
+			push(list, list_b, 'b');
+			less++;
+			size--;
+		}
+		else
+		{
+			if (get_node_index(*list, size - 1) == less || \
+				get_node_index(*list, size - 2) == less)
+						reverse_rotate(list, 'a');
+			else
+				rotate(list, 'a');
+		}
+		aux = *list;
+	}
+	return aux->index;
+}
