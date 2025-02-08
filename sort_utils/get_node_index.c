f129af3d236f74f838c4e301c5b6af3e44015850
palomaprates <paloma.prates108@gmail.com>
2025-02-08 10:28:51 -0300

    create get node index function

---
+++ sort_utils/get_node_index.c
@@ -0,0 +1,18 @@
+#include "../push_swap.h"
+
+int	get_node_index(Node *list, int index)
+{
+	int	i;
+	int	size;
+
+	i = 0;
+	size = get_list_size(list);
+	while (i < size)
+	{
+		if (i == index)
+			return list->index;
+		i++;
+		list = list->next;
+	}
+	return -1;
+}
