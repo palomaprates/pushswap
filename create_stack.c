386ed4244699ec2070bac50ea5a586787d6ca610
palomaprates <paloma.prates108@gmail.com>
2025-04-04 17:04:22 +0100

    fix function

--- create_stack.c
+++ create_stack.c
@@ -6,12 +6,26 @@
 /*   By: pprates- <pprates-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 10:39:21 by pprates-          #+#    #+#             */
-/*   Updated: 2025/04/04 15:20:28 by pprates-         ###   ########.fr       */
+/*   Updated: 2025/04/04 16:54:45 by pprates-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	valid_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] == '+' || str[i] == '-') && \
+		(str[i + 1] == '0' && str[i + 1]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 int	create_stack(int size, char **argv, t_node **list)
 {
 	long long	result;
@@ -25,7 +39,8 @@ int	create_stack(int size, char **argv, t_node **list)
 			ft_lstclear(list);
 			return (0);
 		}
-		if (!(is_str_number(argv[i])) || ft_strlen(argv[i]) > 11)
+		if (!(is_str_number(argv[i])) || !valid_number(argv[i]) || \
+		ft_strlen(argv[i]) > 11)
 		{
 			ft_lstclear(list);
 			return (0);
